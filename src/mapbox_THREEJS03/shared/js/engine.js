cff1dece22496326412d083c116da52c3444334d
hiroshi.koi@i-studio.co.jp <hiroshi.koi@i-studio.co.jp>
2018-04-10 16:11:00 +0900

    add mapbox_THREEJS03
    処理最適化版01

---
+++ src/mapbox_THREEJS03/shared/js/engine.js
@@ -0,0 +1,195 @@
+/*
+	engine.js
+*/
+
+(function(){
+
+	var _world;
+	var _plane;
+	var _rgbImage;
+	var _canvas, _ctx;
+
+	var _zoom = 15;
+	var _lng = 139.760668;
+	var _lat = 35.67326;
+
+	$('#siteBody').addClass('open');
+
+	//long: 180
+	//lat:90
+
+	var _zoom = 14;
+	var _lng = 131.0300536;
+	var _lat = -25.3493748;
+	var _scaleHeight = 64.0 / Math.pow( 2, 24 - _zoom );
+
+
+	var _x = long2tile( _lng, _zoom );
+	var _y = lat2tile( _lat, _zoom );
+	var _timeStamp = new Date().getTime();
+
+	var _dataSet = [];
+	var _grid = 2;		//	pow2
+	var _gridSize = 100;
+
+	var _mainCanvas, _mainCtx;
+	var _subCanvas, _subCtx;
+
+	var _api = 'https://api.mapbox.com';
+	var _accessToken = 'pk.eyJ1IjoibnVsbGRlc2lnbiIsImEiOiJjamZrcGdtbnowODdlMndzMmE2ZHc5anlrIn0.fsWuly11P-SWfGz9VntnSg';
+	var _url = _api + '/v4/mapbox.terrain-rgb/'+_zoom+'/'+_x+'/'+_y+'.pngraw?access_token=' + _accessToken;
+	var _imgUrl = _api + '/v4/mapbox.satellite/'+_zoom+'/'+_x+'/'+_y+'@2x.png256?access_token=' + _accessToken;
+
+
+	//	
+	init();
+	update();
+
+	//	THREE.js with mapbox
+	setData( _x, _y, _zoom );
+	load();
+
+
+
+	/*
+		Function
+
+
+
+	*/
+
+	function init(){
+		_world = new world('webglView');
+		_world.render();
+
+		//	texture
+		_mainCanvas = document.createElement('canvas');
+		_mainCanvas.width = 512 * _grid;
+		_mainCanvas.height = 512 * _grid;
+		_mainCtx = _mainCanvas.getContext('2d');
+
+		//	rgb
+		_subCanvas = document.createElement('canvas');
+		_subCanvas.width = 256 * _grid;
+		_subCanvas.height = 256 * _grid;
+		_subCtx = _subCanvas.getContext('2d');
+	}
+
+	function update(){
+		var _current = new Date().getTime();
+		var _duration = _current - _timeStamp;
+		_timeStamp = _current;
+		var _fps = Math.floor( 1000 / _duration );
+		$('h2').text('FPS: ' + _fps);
+
+		window.requestAnimationFrame( update );
+	}
+
+	function setData( _x, _y, _zoom ){
+		for( var i = 0; i < _grid; i++ ){
+			for( var j = 0; j < _grid; j++ ){
+				var _obj = {};
+				_obj.x = _x + i - Math.floor( _grid * 0.5 );
+				_obj.y = _y + j - Math.floor( _grid * 0.5 );
+				_obj.url = _api + '/v4/mapbox.terrain-rgb/'+_zoom+'/'+_obj.x+'/'+_obj.y+'.pngraw?access_token=' + _accessToken;
+				_obj.src = _api + '/v4/mapbox.satellite/'+_zoom+'/'+_obj.x+'/'+_obj.y+'@2x.png256?access_token=' + _accessToken;
+				_obj.index = {x:i,y:j};
+				_obj.zoom = _zoom;
+				_dataSet.push( _obj );
+			}
+		}
+
+	}
+
+	function load(){
+		if( _dataSet.length ){
+			var _data = _dataSet.pop();
+			var _img = new Image();
+			_img.crossOrigin = 'anonymous';
+			_img.onload = function(e){
+				var _t0 = this;
+				var __img = new Image();
+				__img.crossOrigin = 'anonymous';
+				__img.onload = function(e){
+					var _t1 = this;
+
+					var _x = _data.index.x;
+					var _y = _data.index.y;
+					_subCtx.drawImage( _t0, _x * 256, _y * 256 );
+					_mainCtx.drawImage( _t1, _x * 512, _y * 512 )
+
+					load();
+				}
+				__img.src = _data.src;
+			}
+			_img.src = _data.url;
+		} else {
+			createPlane();
+		}
+	}
+
+	function createPlane(){
+
+		//	rgb
+		var _imagedata = _subCtx.getImageData(0, 0, _subCanvas.width, _subCanvas.height);
+		var _data = _imagedata.data;
+
+		var _geometry = new THREE.PlaneGeometry( _gridSize * _grid, _gridSize * _grid, _subCanvas.width-1, _subCanvas.height-1 );
+		_geometry.rotateX( - Math.PI * 0.5 );
+
+		var len = _geometry.vertices.length;
+		for( var i = 0; i < len; i++ ){
+			var _r = _data[ i * 4 + 0 ];
+			var _g = _data[ i * 4 + 1 ];
+			var _b = _data[ i * 4 + 2 ];
+			var _a = _data[ i * 4 + 3 ];
+			var _y = - 10000 + ((_r * 256 * 256 + _g * 256 + _b) * 0.1);
+			_geometry.vertices[i].y = _y * _scaleHeight;
+		}
+
+		_geometry.verticesNeedUpdate = true;
+		_geometry.normalsNeedUpdate = true;
+
+		_geometry.computeBoundingBox();
+		_geometry.computeBoundingSphere();
+		_geometry.computeFaceNormals();
+
+		var _t1 = _mainCanvas.toDataURL("image/png");
+		var _t0 = _subCanvas.toDataURL("image/png");
+
+		var _texture = new THREE.TextureLoader().load( _t1 );
+		var _mapHeight = new THREE.TextureLoader().load( _t0 );
+		var _material = new THREE.MeshStandardMaterial({
+			map: _texture,
+			color: 0xFFFFFF,
+			roughness: 1.0,
+			metalness: 0.0,
+			bumpMap: _mapHeight,
+			bumpScale: 16,
+			side: THREE.DoubleSide,
+			wireframe: false
+		});
+		var _plane = new THREE.Mesh( _geometry, _material );
+		_world.add( _plane );
+
+		//	
+		var _vertices = _geometry.vertices.length;
+		var _faces = _geometry.faces.length;
+		$('#siteFoot p').text( 'Vertex: ' + _vertices + ', Faces: ' + _faces );
+
+	}
+
+	function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+ 	function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+
+	function tile2long(x,z) {
+		return (x/Math.pow(2,z)*360-180);
+	}
+	function tile2lat(y,z) {
+		var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
+		return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+	}
+
+})();
+
+
