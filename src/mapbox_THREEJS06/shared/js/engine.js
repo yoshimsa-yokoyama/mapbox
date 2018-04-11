914db00c9296e25a8d97cea538707de1a06785e8
hiroshi.koi@i-studio.co.jp <hiroshi.koi@i-studio.co.jp>
2018-04-11 15:30:06 +0900

    add mapbox_THREEJS06
    一部最適化

---
+++ src/mapbox_THREEJS06/shared/js/engine.js
@@ -0,0 +1,300 @@
+/*
+	engine.js
+*/
+
+(function(){
+
+	var _world;
+	var _plane;
+	var _isLite = true;
+
+	//	kagura
+	var _zoom = 13;
+	var _lng = 138.726655;
+	var _lat = 36.862498;
+
+	//	hakuba	
+	// var _zoom = 11;
+	// var _lng = 137.859266;
+	// var _lat = 36.695374;
+
+	//	niseko
+	// var _zoom = 14;
+	// var _lng = 140.657851;
+	// var _lat = 42.875400;
+
+	//	AirsRock
+	// var _zoom = 14;
+	// var _lng = 131.034774;
+	// var _lat = -25.344255;
+
+	var _x = long2tile( _lng, _zoom );
+	var _y = lat2tile( _lat, _zoom );
+	var _timeStamp = new Date().getTime();
+
+	var _dataSet = [];
+	var _grid = 2;		//	pow2
+	var _gridSize = 100;
+	var _scaleHeight = 64.0 / Math.pow( 2, 24 - _zoom ) * _gridSize / 100;
+
+	var _mainCanvas, _mainCtx;
+	var _subCanvas, _subCtx;
+	var _imageList = [];
+
+	var _api = 'https://api.mapbox.com';
+	var _accessToken = 'pk.eyJ1IjoibnVsbGRlc2lnbiIsImEiOiJjamZrcGdtbnowODdlMndzMmE2ZHc5anlrIn0.fsWuly11P-SWfGz9VntnSg';
+
+	//	contents start.
+	$('#siteBody').addClass('open');
+
+	init();
+	setData( _x, _y, _zoom );
+	load();
+	//	update();
+
+	function init(){
+		_world = new world('webglView');
+		//_world.render();
+
+		//	texture
+		_mainCanvas = document.createElement('canvas');
+		_mainCanvas.width = 512 * _grid;
+		_mainCanvas.height = 512 * _grid;
+		_mainCtx = _mainCanvas.getContext('2d');
+
+		//	rgb
+		_subCanvas = document.createElement('canvas');
+		_subCanvas.width = 256 * _grid;
+		_subCanvas.height = 256 * _grid;
+		_subCtx = _subCanvas.getContext('2d');
+	}
+
+	function update(){
+		var _current = new Date().getTime();
+		var _duration = _current - _timeStamp;
+		_timeStamp = _current;
+		var _fps = Math.floor( 1000 / _duration );
+		$('h2').text('FPS: ' + _fps);
+
+		window.requestAnimationFrame( update );
+	}
+
+	function setData( _x, _y, _zoom ){
+		for( var i = 0; i < _grid; i++ ){
+			for( var j = 0; j < _grid; j++ ){
+				var _obj = {};
+				_obj.x = _x + i - Math.floor( _grid * 0.5 );
+				_obj.y = _y + j - Math.floor( _grid * 0.5 );
+				_obj.url = _api + '/v4/mapbox.terrain-rgb/'+_zoom+'/'+_obj.x+'/'+_obj.y+'.pngraw?access_token=' + _accessToken;
+				_obj.src = _api + '/v4/mapbox.satellite/'+_zoom+'/'+_obj.x+'/'+_obj.y+'@2x.png256?access_token=' + _accessToken;
+				_obj.index = {x:i,y:j};
+				_obj.zoom = _zoom;
+				_dataSet.push( _obj );
+			}
+		}
+	}
+
+	function load(){
+		if( _dataSet.length ){
+			var _data = _dataSet.pop();
+			var _img = new Image();
+			_img.crossOrigin = 'anonymous';
+			_img.onload = function(e){
+				var _t0 = this;
+				var __img = new Image();
+				__img.crossOrigin = 'anonymous';
+				__img.onload = function(e){
+					var _t1 = this;
+					var _x = _data.index.x;
+					var _y = _data.index.y;
+
+					_imageList.push({
+						x: _x,
+						y: _y,
+						img0: _t0,
+						img1: _t1
+					});
+
+					setTimeout( load, 100 );
+				}
+				__img.src = _data.src;
+			}
+			_img.src = _data.url;
+		} else {
+			createImage();
+			//	resizeCanvas();
+			createPlane();
+		}
+	}
+
+	function createImage(){
+		var len = _imageList.length;
+		for( var i = 0; i < len; i++ ){
+			var _data = _imageList[i];
+			var _x = _data.x;
+			var _y = _data.y;
+			var _t0 = _data.img0;
+			var _t1 = _data.img1;
+			_subCtx.drawImage( _t0, _x * 256, _y * 256 );
+			_mainCtx.drawImage( _t1, _x * 512, _y * 512 );
+		}
+	}
+
+	function resizeCanvas(){
+		var _canvas = document.createElement('canvas');
+		_canvas.width = 256;
+		_canvas.height = 256;
+		var _ctx = _canvas.getContext('2d');
+		_ctx.drawImage( _subCanvas, 0, 0, _subCanvas.width, _subCanvas.height, 0, 0, 256, 256 );
+		_subCanvas = _canvas;
+		_subCtx = _ctx;
+	}
+
+	function createPlane(){
+		var _imagedata = _subCtx.getImageData(0, 0, _subCanvas.width, _subCanvas.height);
+		var _data = _imagedata.data;
+
+		var _geometry = new THREE.PlaneGeometry( _gridSize * _grid, _gridSize * _grid, _subCanvas.width-1, _subCanvas.height-1 );
+		_geometry.rotateX( - Math.PI * 0.5 );
+
+		var _posMin = Number.POSITIVE_INFINITY;
+
+		var len = _geometry.vertices.length;
+		for( var i = 0; i < len; i++ ){
+			var _r = _data[ i * 4 + 0 ];
+			var _g = _data[ i * 4 + 1 ];
+			var _b = _data[ i * 4 + 2 ];
+			var _a = _data[ i * 4 + 3 ];
+			var _posy = - 10000 + ((_r * 256 * 256 + _g * 256 + _b) * 0.1);
+			_geometry.vertices[i].y = _posy * _scaleHeight;
+
+			_posMin = _posMin < _posy ? _posMin : _posy;
+		}
+		for( var i = 0; i < len; i++ ){
+			_geometry.vertices[i].y -= _posMin * _scaleHeight;
+		}
+
+		_geometry.verticesNeedUpdate = true;
+		_geometry.normalsNeedUpdate = true;
+		_geometry.computeBoundingBox();
+		_geometry.computeBoundingSphere();
+		_geometry.computeFaceNormals();
+
+		var _texture = new THREE.Texture( _mainCanvas );
+ 		_texture.needsUpdate = true; 
+		var _mapHeight = new THREE.Texture( _subCanvas );
+ 		_mapHeight.needsUpdate = true; 
+
+
+		var _material = new THREE.MeshStandardMaterial({
+			map: _texture,
+			color: 0xFFFFFF,
+			roughness: 1.0,
+			metalness: 0.0,
+			bumpMap: _mapHeight,
+			bumpScale: 16,
+			side: THREE.DoubleSide,
+			wireframe: false
+		});
+		var _plane = new THREE.Mesh( _geometry, _material );
+		_world.add( _plane );
+
+		//	
+		var _vertices = _geometry.vertices.length;
+		var _faces = _geometry.faces.length;
+		$('#siteFoot p').text( 'Vertex: ' + _vertices + ', Faces: ' + _faces );
+
+		//	check
+		var _poslist = [
+			[_lat, _lng],
+			[-25.352173, 131.033198],
+			[-25.342787, 131.021568],
+			[-25.358921, 131.017362]
+		];
+
+		var len = _poslist.length;
+		for( var i = 0; i < len; i++ ){
+			var _pos = fromLatLngToPoint( _poslist[i][0], _poslist[i][1], _zoom);
+			var _localX = ( _pos.x - ( _x * 256 ) );
+			var _localY = ( _pos.y - ( _y * 256 ) );
+			var _parX = _localX / 256;
+			var _parY = _localY / 256;
+			var _geometry = new THREE.PlaneGeometry( 3, 3 );
+			_geometry.rotateX( - Math.PI * 0.5 );
+			var _material = new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: true});
+			var _offset = _grid%2==0?0.5:0;
+
+			var _pos = {x:0,y:0,z:0};
+			_pos.x = _parX * _gridSize - Math.floor( _gridSize * 0.5 ) + _gridSize * _offset;
+			_pos.y = 0;
+			_pos.z = _parY * _gridSize - Math.floor( _gridSize * 0.5 ) + _gridSize * _offset;
+
+			//	pin
+			var _pin = new THREE.Mesh( _geometry, _material );
+			_pin.position.x = _pos.x;
+			_pin.position.y = _pos.y;
+			_pin.position.z = _pos.z;
+			_world.add( _pin );
+
+			//	Raycaster
+			var _start = new THREE.Vector3( _pos.x, 0, _pos.z );
+			var _dir = new THREE.Vector3( 0, 1, 0 );
+			var _ray = new THREE.Raycaster( _start, _dir );
+			var objs = _ray.intersectObjects( [_plane] );
+			var _pinOffset = 20;
+
+			if (objs.length > 0) {
+				_pin.position.y = objs[0].distance + _pinOffset;
+			}
+
+			var _geometry = new THREE.Geometry();
+			_geometry.vertices[0] = new THREE.Vector3( 0, 0, 0 );
+			_geometry.vertices[1] = new THREE.Vector3( 0, _pinOffset, 0 );
+			var _material = new THREE.LineBasicMaterial({color: 0xFF0000});
+			var _line = new THREE.Line( _geometry, _material );
+			_line.position.x = _pos.x;
+			_line.position.y = _pin.position.y - _pinOffset;
+			_line.position.z = _pos.z;
+			_world.add( _line );
+		}
+
+		_world.render();
+		update();
+	}
+
+	function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+ 	function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+	function tile2long(x,z) {
+		return (x/Math.pow(2,z)*360-180);
+	}
+	function tile2lat(y,z) {
+		var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
+		return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+	}
+	function fromLatLngToPoint( _lat, _lng, z ){
+		var L = 85.0511287798066;
+		var PI = Math.PI;
+		var _x = Math.pow( 2, z + 7 ) * ( _lng / 180 + 1 );
+		var _sinLat = Math.sin( PI / 180 * _lat );
+		var _sinL = Math.sin( PI / 180 * L );
+		var _y = Math.pow( 2, z + 7 ) / PI * 
+		(
+			- 1/2 * Math.log( (1.0 + _sinLat)/(1.0 - _sinLat ) )
+		+
+			1/2 * Math.log( (1.0 + _sinL)/(1.0 - _sinL ) )
+		);
+		return {x:_x>>0,y:_y>>0};
+	}
+
+	/*
+		参考URL
+		https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+		http://www.trail-note.net/tech/coordinate/
+		http://hosohashi.blog59.fc2.com/blog-entry-5.html
+		https://ja.wikipedia.org/wiki/逆三角関数
+		https://mathtrain.jp/invhyp
+		https://www.mapbox.com/blog/tags/terrain/
+	*/
+})();
+
+
---
+++ src/mapbox_THREEJS06/shared/js/world.js
@@ -0,0 +1,131 @@
+/*
+	world.js
+	auth: TrekTrack @HAKUHODO i-studio, Inc.
+	url: http://trektrack.jp/
+*/
+
+var world = (function(){
+
+	function world( e )
+	{
+		this.scene;
+		this.camera;
+		this.focus;
+		this.renderer;
+		this.ambient;
+		this.controls;
+
+		this.cameraMode;
+
+		this.init( e );
+
+		var _t = this;
+		window.onresize = function(){
+			_t.resize();
+		};
+
+		this.render();
+	}
+
+	world.prototype = {
+		init : function( e )
+		{
+			var width  = window.innerWidth;
+			var height = window.innerHeight;
+			this.scene = new THREE.Scene();
+			this.scene.fog = new THREE.Fog( 0x000000, 500, 1000 );
+
+			this.camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000 );
+			//this.camera = new THREE.OrthographicCamera( - width * 0.5, width * 0.5, height * 0.5, - height * 0.5, 0.1, 1000 );
+			this.camera.position.set( 100, 140, 100 );
+			this.focus = new THREE.Vector3(0,0,0);
+
+			this.cameraMode = false;
+
+			this.renderer = new THREE.WebGLRenderer({ antialias: true });
+			this.renderer.setSize( width, height  );
+			this.renderer.setClearColor( 0x000000 );
+
+			document.getElementById( e ).appendChild(this.renderer.domElement);
+
+			this.ambient = new THREE.AmbientLight( 0x999999 );
+			this.scene.add( this.ambient );
+
+			this.directional = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+			this.directional.position.set( 45, 35, 105 );
+			this.scene.add( this.directional );
+
+			this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+			this.controls.autoRotate = true;
+			this.controls.autoRotateSpeed = 0.125;
+			
+			this.controls.enableDamping = true;
+			this.controls.dampingFactor = 0.15;
+			this.controls.enableZoom = false;
+
+			this.controls.enabled = true;
+			this.controls.target = this.focus;
+
+			// this.controls.minDistance = 80;
+			// this.controls.maxDistance = 120;
+
+			// this.controls.minPolarAngle = 0; // radians
+			// this.controls.maxPolarAngle = Math.PI * 0.45; // radians
+
+		},
+		render : function()
+		{
+			var _t = this;
+			_t.controls.update();
+			_t.camera.lookAt( _t.focus );
+			_t.renderer.render( _t.scene, _t.camera );
+			window.requestAnimationFrame( function(){	_t.render();	} );
+		},
+		resize : function()
+		{
+			var width  = window.innerWidth;
+			var height = window.innerHeight;
+			this.renderer.setSize( width, height );
+			if( this.camera.aspect )
+			{
+				this.camera.aspect = width / height;
+			} else {
+				this.camera.left = - width * 0.5;
+				this.camera.right = width * 0.5;
+				this.camera.bottom = - height * 0.5;
+				this.camera.top = height * 0.5;
+			}
+			this.camera.updateProjectionMatrix();
+		},
+		getWorldToScreen2D : function( _mesh )
+		{
+			var vector = new THREE.Vector3();
+			var widthHalf = 0.5*this.renderer.context.canvas.width;
+			var heightHalf = 0.5*this.renderer.context.canvas.height;
+			_mesh.updateMatrixWorld();
+			vector.setFromMatrixPosition(_mesh.matrixWorld);
+			vector.project(this.camera);
+			vector.x = ( vector.x * widthHalf ) + widthHalf;
+			vector.y = - ( vector.y * heightHalf ) + heightHalf;
+			return { 
+			    x: vector.x,
+			    y: vector.y
+			};
+		},
+		add : function( e )
+		{
+			this.scene.add( e );
+		},
+		remove : function( e )
+		{
+			this.scene.remove( e );
+		},
+		setCameraMode : function( e ){
+			this.cameraMode = e;
+			this.controls.enabled = !e;
+			if( e ){	this.camera.position.set( 0, 0, 100 );	}
+			if( !e ){	this.camera.position.set( 0, 0, 100 );	}
+		}
+	}
+	return world;
+})();
